0b622fb0379397ecee8709b49f754730ddc0d784
Jakob Brand <brandjak@hu-berlin.de>
2022-06-29 18:27:51 +0200

    Added new controller for custom robot

---
+++ controllers/advanced_genetic_algorithm/build/release/advanced_genetic_algorithm.d
@@ -0,0 +1,7 @@
+build/release/advanced_genetic_algorithm.o: advanced_genetic_algorithm.c \
+ /home/jakob/webots/include/controller/c/webots/distance_sensor.h \
+ /home/jakob/webots/include/controller/c/webots/types.h \
+ /home/jakob/webots/include/controller/c/webots/motor.h \
+ /home/jakob/webots/include/controller/c/webots/receiver.h \
+ /home/jakob/webots/include/controller/c/webots/robot.h \
+ /home/jakob/webots/include/controller/c/webots/nodes.h
---
+++ controllers/advanced_genetic_algorithm_dog/build/release/advanced_genetic_algorithm_dog.d
@@ -0,0 +1,8 @@
+build/release/advanced_genetic_algorithm_dog.o: \
+ advanced_genetic_algorithm_dog.c \
+ /home/jakob/webots/include/controller/c/webots/distance_sensor.h \
+ /home/jakob/webots/include/controller/c/webots/types.h \
+ /home/jakob/webots/include/controller/c/webots/motor.h \
+ /home/jakob/webots/include/controller/c/webots/receiver.h \
+ /home/jakob/webots/include/controller/c/webots/robot.h \
+ /home/jakob/webots/include/controller/c/webots/nodes.h
